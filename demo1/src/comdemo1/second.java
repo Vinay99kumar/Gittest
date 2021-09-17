0f4570f42ed3e0356e36d7714bc5f9b85fd9fb45
abhiagnihotri012 <abhihackera@gmail.com>
2021-09-17 16:53:52 +0530

    neosoft noida team commit1

--- demo1/src/comdemo1/second.java
+++ demo1/src/comdemo1/second.java
@@ -4,6 +4,7 @@ public class second {
 
 	public static void main(String[] args) {
 		System.out.println("welcome java second class");
+		System.out.println("welcome java edited by abhishek 9/17/21");
 
 	}
 
